1382b97628b959a518db05b5165f21fa3779e3fe
athensai <122765545+athensai@users.noreply.github.com>
2023-05-08 13:29:02 -0400

    Update 🧪_Focus_Group.py

--- pages/🧪_Focus_Group.py
+++ pages/🧪_Focus_Group.py
@@ -12,7 +12,7 @@ if st.button("Message Test"):
     sys = [{
         "role": "system",
         "content": "Simulate a long, American focus group transcript for the following candidate message." 
-        "Include 10 participants, make them diverse in geography, partisanship, gender, race, age, and occupation, and describe them briefly.
+        "Include 10 participants, make them diverse in geography, partisanship, gender, race, age, and occupation, and describe them briefly."
         "BE REALISTIC. Some of the people should be critical and disagree. Do not make it fake or overly positive. Markdown:" 
     }]
     transcript = chat(speech, messages=sys, model="gpt-4", max_tokens=1000)
